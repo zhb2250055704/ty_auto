9aac5d082edce2354418d1c6b7b720eb0af258bd
zhanghanbin <zhanghanbin@tuyoogame.com>
2023-01-29 10:59:10 +0800

    [empty] initial commit

--- dog.py
+++
@@ -1,9 +0,0 @@
-import lackey
-import subprocess
-import time
-# 打开perfdog脚本
-def perfdog():
-    #subprocess.Popen(r'C:\Users\lh\Desktop\PerfDog(v8.0.221052)\PerfDog.exe', cwd=r'C:\Users\lh\Desktop\PerfDog(v8.0.221052)')
-    time.sleep(1)
-    lackey.click('photo/dadian.png')
-    time.sleep(1)
