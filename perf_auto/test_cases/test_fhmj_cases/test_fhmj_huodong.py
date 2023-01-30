c4ce858a3ac3328e2a36b0a15ca9da962537a686
zhanghanbin <zhanghanbin@tuyoogame.com>
2023-01-30 11:19:24 +0800

    【活动】方法修改

--- perf_auto/test_cases/test_fhmj_cases/test_fhmj_huodong.py
+++ perf_auto/test_cases/test_fhmj_cases/test_fhmj_huodong.py
@@ -7,17 +7,17 @@ poco = UnityPoco()
 #活动
 def huodong():
     poco("ui_hall_down_gongjulan_huodong").click()
-        for i in range(0,20):
-            swipe(Template(r"../perf_auto/images/fhmj_img/tuodong1.png", record_pos=(-0.322, 0.012), resolution=(2400, 1080)), vector=[0.0062, -0.358])
-            swipe(Template(r"../perf_auto/images/fhmj_img/tuodong2.png", record_pos=(-0.316, 0.015), resolution=(2400, 1080)), vector=[0.0004, 0.329])
-        for i in range(0,3):
-            poco(text="番型收集").click()
-            poco(text="杠上杠新玩法").click()
-            poco(text="疯狂十三幺").click()
-            poco(text="每日任务").click()
-            poco(text="麻将无双杯").click()
-            swipe(Template(r"t../perf_auto/images/fhmj_img/tuodong3.png", record_pos=(-0.317, 0.0), resolution=(2400, 1080)), vector=[-0.0058, -0.3408])
-            poco(text="川麻雀神杯").click()
-            poco(text="麻将话费").click()
-            swipe(Template(r"../perf_auto/images/fhmj_img/tuodong4.png", record_pos=(-0.262, 0.012), resolution=(2400, 1080)), vector=[0.0084, 0.2987])
-        poco("ui_close").click()
+    for i in range(0,20):
+        swipe(Template(r"../perf_auto/images/fhmj_img/tuodong1.png", record_pos=(-0.322, 0.012), resolution=(2400, 1080)), vector=[0.0062, -0.358])
+        swipe(Template(r"../perf_auto/images/fhmj_img/tuodong2.png", record_pos=(-0.316, 0.015), resolution=(2400, 1080)), vector=[0.0004, 0.329])
+    for i in range(0,3):
+        poco(text="番型收集").click()
+        poco(text="杠上杠新玩法").click()
+        poco(text="疯狂十三幺").click()
+        poco(text="每日任务").click()
+        poco(text="麻将无双杯").click()
+        swipe(Template(r"t../perf_auto/images/fhmj_img/tuodong3.png", record_pos=(-0.317, 0.0), resolution=(2400, 1080)), vector=[-0.0058, -0.3408])
+        poco(text="川麻雀神杯").click()
+        poco(text="麻将话费").click()
+        swipe(Template(r"../perf_auto/images/fhmj_img/tuodong4.png", record_pos=(-0.262, 0.012), resolution=(2400, 1080)), vector=[0.0084, 0.2987])
+    poco("ui_close").click()
