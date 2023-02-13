6b8e0f33cc43fed8eb3463e76d410da3bcc9e029
zhanghanbin <zhanghanbin@tuyoogame.com>
2023-02-13 17:01:06 +0800

    富豪麻将【夺宝】优化：增加print输出信息

--- perf_auto/test_cases/test_fhmj_cases/test_fhmj_duobao.py
+++ perf_auto/test_cases/test_fhmj_cases/test_fhmj_duobao.py
@@ -5,6 +5,7 @@ from poco.drivers.unity3d import UnityPoco
 poco = UnityPoco()
 
 def duobao():
+    print('-----夺宝场景开始运行-----')
     poco("ui_hall_down_gongjulan_draw").click()
     poco(text="夺宝").wait_for_appearance()
     poco("ui_currencyBar_4").child("img_con").click()
@@ -12,6 +13,7 @@ def duobao():
         poco("ui_btn_add1").click()
     poco("ui_btn_buy").click()
     poco("ui_bottom_text").click()
+    print('-----完成抽奖币购买-----')
     if poco(name="Image (2)").exists():
         poco("btn_close").click()
     for i in range(1,6):
@@ -20,7 +22,13 @@ def duobao():
         if poco("ui_btn_share").exists():
             poco("ui_btn_confirm").click()
             poco("ui_btn_confirm").click()
+            print(f'-----完成第{i}次抽奖-----')
         else:
             poco("ui_btn_again").exists()
             poco("ui_btn_confirm").click()
+            print(f'-----完成第{i}次抽奖-----')
     poco("ui_btn_return").click()
+    print('-----夺宝场景运行结束-----')
+
+if __name__ == '__main__':
+    duobao()
